f8c6b2219d30a47a3d55e79a7438b2bb5001ab5b
canyu3411 <canyu3411@gmail.com>
2025-09-25 16:16:24 +0100

    MergeTwoLists.py

--- Linked Lists/reverseList.py
+++ Linked Lists/reverseList.py
@@ -1,9 +1,17 @@
-def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    prev = None
-    current = head
-    while current:
-        after = current.next
-        current.next = prev
-        prev = current
-        current = after
-    return prev
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+        while current:
+            after = current.next
+            current.next = prev
+            prev = current
+            current = after
+        return prev
