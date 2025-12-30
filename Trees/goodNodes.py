57533d9cc6717416c2ae810fb347e67682ccd46f
canyu3411 <canyu3411@gmail.com>
2025-12-30 14:27:05 +0000

    Created goodNodes.py

---
+++ Trees/goodNodes.py
@@ -0,0 +1,35 @@
+def goodNodes(self, root: TreeNode) -> int:
+    goodCount = 0
+    def traversal(node, max):
+        nonlocal goodCount
+        if not node:
+            return
+        if node.val >= max:
+            max = node.val
+            goodCount += 1
+        traversal(node.left, max)
+        traversal(node.right, max)
+    traversal(root, -10001)
+    return goodCount
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
