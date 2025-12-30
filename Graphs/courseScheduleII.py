fbdeb8ce9df119e6cc00a1ff9a804dd9f002014a
canyu3411 <canyu3411@gmail.com>
2025-12-30 15:21:06 +0000

    Created courseScheduleII.py

---
+++ Graphs/courseScheduleII.py
@@ -0,0 +1,61 @@
+from typing import List
+def findOrder(numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    roadmap = []
+    coursePrereqs = {prerequisite : [] for prerequisite in range(numCourses)}
+    for prerequisite in prerequisites:
+        coursePrereqs[prerequisite[0]].append(prerequisite[1])
+
+    provenCourses = set()
+    visiting = set()
+    def canDo(course):
+        if not coursePrereqs[course] and not course in provenCourses:
+            provenCourses.add(course)
+            roadmap.append(course)
+            return True
+        if course in provenCourses:
+            return True
+        if course in visiting:
+            return False
+        visiting.add(course)
+        for prerequisite in coursePrereqs[course]:
+            if not canDo(prerequisite):
+                return False
+        provenCourses.add(course)
+        visiting.remove(course)
+        roadmap.append(course)
+        return True
+    for course in range(numCourses):
+        if not canDo(course):
+            return []
+    return roadmap
+
+print(findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+
+        output = []
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return []
+        return output
