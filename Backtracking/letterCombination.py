c513324be6a63543326c8cdb79597c792df95856
canyu3411 <canyu3411@gmail.com>
2026-01-03 12:41:17 +0000

    Created letterCombination.py

---
+++ Backtracking/letterCombination.py
@@ -0,0 +1,51 @@
+from typing import List
+def letterCombinations(digits: str) -> List[str]:
+    answer = []
+    numToLetter = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+    }
+    def backtracking(index, curr):
+        if index == len(digits):
+            answer.append(curr)
+            return
+        for char in numToLetter[digits[index]]:
+            curr += char
+            backtracking(index+1, curr[:])
+            curr = curr[:-1]
+    backtracking(0, "")
+    return answer
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
+    
+
