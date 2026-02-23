43c96fefa42604941264fea8db22499c4ffc5d6d
canyu3411 <canyu3411@gmail.com>
2026-02-23 12:29:20 +0800

    Created wordSearch.py

--- Backtracking/wordSearch.py
+++ Backtracking/wordSearch.py
@@ -1,31 +1,29 @@
+from typing import List
 def exist(self, board: List[List[str]], word: str) -> bool:
+    # Looks like dfs but backtrack visited nodes
     ROWS = len(board)
     COLS = len(board[0])
-    path = set()
-
-    def dfs(row, col, letter_index):
-        if letter_index == len(word):
+    def inBounds(row, col):
+        if 0 <= row < ROWS and  0 <= col < COLS:
             return True
-        elif (row < 0 or row >= ROWS or
-            col >= COLS or col < 0 or
-            board[row][col] != word[letter_index] or
-            (row, col) in path):
-            return False
-        
-        path.add((row, col))
-        letter_index += 1
-        answer = (
-        dfs(row+1, col, letter_index) or 
-        dfs(row-1, col, letter_index) or
-        dfs(row, col+1, letter_index) or
-        dfs(row, col-1, letter_index))
-        path.remove((row, col))
-        return answer
+        else: return False
+    def backtrack(row, col, visited, wordIndex):
+        if wordIndex == len(word):
+            return True
+        directions = [(row+1, col), (row-1, col), (row, col-1), (row, col+1)]
+        for direction in directions:
+            if (inBounds(direction[0], direction[1]) and not direction in visited and 
+                board[direction[0]][direction[1]] == word[wordIndex]):
+                visited.add((direction[0], direction[1]))
+                if backtrack(direction[0], direction[1], visited, wordIndex+1):
+                    return True
+                visited.remove((direction[0], direction[1]))
     
-
-    for col in range(COLS):
-        for row in range(ROWS):
-            if dfs(row, col, 0):
-                return True
+    for row in range(ROWS):
+        for col in range(COLS):
+            if board[row][col] == word[0]:
+                visited = set()
+                visited.add((row, col))
+                if backtrack(row, col, visited, 1):
+                    return True
     return False
-    
