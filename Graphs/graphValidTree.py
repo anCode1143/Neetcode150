14e899f1aeb31c87611e9605c0033acd4faec1fc
canyu3411 <canyu3411@gmail.com>
2026-02-27 14:59:18 +0800

    Created graphValidTree.py

--- Graphs/graphValidTree.py
+++ Graphs/graphValidTree.py
@@ -1,24 +1,37 @@
+from typing import List
+
 def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
-    def dfs(node, prev):
-        if node in visit:
+    adjList = [[] for _ in range(n)]
+    for edge in edges:
+        adjList[edge[0]].append(edge[1])
+        adjList[edge[1]].append(edge[0])
+    
+    def dfs(node, visited, justVisited):
+        if node in visited:
             return False
-        visit.add(node)
-        for neighbour in neighbourList[node]:
-            if neighbour == prev:
+        visited.add(node)
+        for child in adjList[node]:
+            if child == justVisited :
                 continue
-            if not dfs(neighbour, node):
+            if not dfs(child, visited, node):
                 return False
         return True
-
-    if not n:
+    
+    visited = set()
+    if dfs(0, visited, None) and len(visited) == n:
         return True
+    else:
+        return False
+# def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+#     adjList = [[] for _ in range(len(edges))]
+#     for edge in edges:
+#         adjList[edge[0]].append(edge[1])
     
-    neighbourList = {i:[] for i in range(n)}
-    for n1, n2 in edges:
-        neighbourList[n1].append(n2)
-        neighbourList[n2].append(n1)
-    
-    visit = set()
-    if dfs(0, -1):
-        return len(visit) == n
-    return False
+#     def dfs(node, visited, justVisited):
+#         if node in visited:
+#             return False
+#         visited.add(node)
+#         for child in adjList[node]:
+#             if not dfs(child, visited):
+#                 return False
+#         return True
